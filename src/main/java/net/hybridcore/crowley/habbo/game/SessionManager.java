0120e9252391538169ba0738ae441370ca410224
Crowley <crowlie@hybridcore.net>
2012-02-13 02:38:46 +0000

    Now use Maven to build.

---
+++ src/main/java/net/hybridcore/crowley/habbo/game/SessionManager.java
@@ -0,0 +1,107 @@
+package net.hybridcore.crowley.habbo.game;
+
+import net.hybridcore.crowley.habbo.beans.Habbo;
+import net.hybridcore.crowley.habbo.message.ServerMessage;
+import org.apache.log4j.Logger;
+import org.jboss.netty.channel.Channel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <crowlie@hybridcore.net> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Crowley.
+ */
+public class SessionManager {
+    private static final Logger logger = Logger.getLogger(SessionManager.class.getName());
+    
+    private Map<Channel, GameSession> sessions = new HashMap<Channel, GameSession>();
+    private Map<Long, GameSession> habbos = new HashMap<Long, GameSession>();
+    private int clientCount = 0;
+    
+    public GameSession addConnection(Channel channel) {
+        GameSession gameSession = new GameSession(channel, ++clientCount);
+        this.sessions.put(channel, gameSession);
+
+        gameSession.start();
+        logger.info("Accepted session (id: " + gameSession.getID() + " ip: "+ gameSession.getIP() + ")");
+
+        return gameSession;
+    }
+    
+    public void removeConnection(Channel channel) {
+        GameSession gameSession = this.getSession(channel);
+        this.sessions.remove(channel);
+
+        if (gameSession.getHabbo() != null) {
+            this.markOffline(gameSession);
+        }
+
+        logger.info("Closed session (id: " + gameSession.getID() + " ip: " + gameSession.getIP() + ")");
+    }
+
+    public GameSession getSession(Channel channel) {
+        if (this.sessions.containsKey(channel)) {
+            return this.sessions.get(channel);
+        }
+
+        return null;
+    }
+    
+    public GameSession getSession(Habbo habbo) {
+        if (this.habbos.containsKey(habbo.getId())) {
+            return this.habbos.get(habbo.getId());
+        }
+
+        return null;
+    }
+    
+    public void markOnline(GameSession gameSession) {
+        if (this.habbos.containsKey(gameSession.getHabbo().getId())) {
+            // Wtf already online...
+            GameSession session = this.habbos.get(gameSession.getHabbo().getId());
+            session.getChannel().disconnect();
+        } else {
+            this.habbos.put(gameSession.getHabbo().getId(), gameSession);
+            updateMessenger(gameSession.getHabbo());
+        }
+    }
+
+    public void markOffline(GameSession gameSession) {
+        if (this.habbos.containsKey(gameSession.getHabbo().getId())) {
+            updateMessenger(gameSession.getHabbo());
+        }
+    }
+
+    private void updateMessenger(Habbo habbo) {
+        // guess we need to alert their friends were here!
+        for (Habbo friend : habbo.getFriends()) {
+            if  (this.isOnline(friend.getId())) {
+                GameSession gameSession = this.getSession(friend);
+
+                gameSession.sendMessage(
+                        new ServerMessage(13)
+                                .append(0)
+                                .append(1) // update count
+                                .append(0)
+                                .append(habbo.getId().intValue())
+                                .appendString(habbo.getName())
+                                .append(true)
+                                .append(habbo.isOnline())
+                                .append(false) //TODO: In Room?
+                                .appendString(habbo.getFigure())
+                                .append(false)
+                                .appendString(habbo.getMotto())
+                                .append("") //TODO: Last online?
+                                .appendString(habbo.getRealName())
+                );
+            }
+        }
+    }
+
+    public boolean isOnline(Long id) {
+        return this.habbos.containsKey(id);
+    }
+}
---
+++ src/main/java/net/hybridcore/crowley/habbo/message/MessageHandler.java
@@ -0,0 +1,80 @@
+package net.hybridcore.crowley.habbo.message;
+
+import net.hybridcore.crowley.habbo.game.GameSession;
+import net.hybridcore.crowley.habbo.message.incoming.*;
+import org.apache.log4j.Logger;
+
+import java.util.HashMap;
+
+/**
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <crowlie@hybridcore.net> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Crowley.
+ */
+public class MessageHandler {
+    private HashMap<Integer, IncomingMessage> messages = new HashMap<Integer, IncomingMessage>();
+    private Logger logger = Logger.getLogger(MessageHandler.class.getName());
+
+    public MessageHandler() {
+        this.registerGlobalHandlers();
+        this.registerSecurityHandlers();
+        this.registerMessenger();
+        this.registerUser();
+    }
+
+    public void registerGlobalHandlers() {
+        this.messages.put(49, new CurrentDate());
+    }
+
+    public void registerSecurityHandlers() {
+        this.messages.put(206, new InitCrypto());
+        this.messages.put(2002, new GenerateKey());
+    }
+
+    public void unregisterSecurityHandlers() {
+        this.messages.remove(206);
+        this.messages.remove(2002);
+    }
+
+    public void registerLoginHandlers() {
+        this.messages.put(415, new SSOTicket());
+    }
+
+    public void unregisterLoginHandlers() {
+        this.messages.remove(415);
+    }
+    
+    public void registerMessenger() {
+        this.messages.put(12, new InitMessenger());
+    }
+
+    public void unregisterMessenger() {
+        this.messages.remove(12);
+    }
+    
+    public void registerUser() {
+        this.messages.put(7, new UserInfo());
+        this.messages.put(8, new UpdateCredits());
+        this.messages.put(26, new SubscriptionStatus());
+        this.messages.put(228, new SoundSettings());
+        this.messages.put(229, new UpdateSoundSettings());
+    }
+
+    public void unregisterUser() {
+        this.messages.remove(7);
+        this.messages.remove(8);
+        this.messages.remove(26);
+        this.messages.remove(228);
+        this.messages.remove(229);
+    }
+
+    public void invoke(GameSession gameSession, ClientMessage message) {
+        if (!this.messages.containsKey(message.getID())) {
+            logger.warn("Unknown message (id: " + message.getID() + " client #" + gameSession.getID() + ")");
+            return;
+        }
+
+        this.messages.get(message.getID()).handle(gameSession, message);
+    }
+}
